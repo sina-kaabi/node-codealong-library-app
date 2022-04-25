65c8eb3549f4d749ab2dded15fc24ce0d62cf098
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-04-25 14:49:03 +0200

    READ: functionality to display book details

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -4,14 +4,27 @@ const router = require("express").Router();
 
 router.get("/books", (req, res, next) => {
     Book.find()
-        .then( (booksArr) => {
-            res.render("books/books-list", {books: booksArr});
+        .then((booksArr) => {
+            res.render("books/books-list", { books: booksArr });
         })
-        .catch( err => {
+        .catch(err => {
             console.log("error getting books from DB", err)
             next(err);
         });
 });
 
+
+router.get("/books/:bookId", (req, res, next) => {
+    const id = req.params.bookId;
+
+    Book.findById(id)
+        .then((bookDetails) => {
+            res.render("books/book-details", bookDetails);
+        })
+        .catch(err => {
+            console.log("error getting book details from DB", err)
+            next(err);
+        });
+})
 
 module.exports = router;
